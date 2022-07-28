04772049c5acf26203492980c5e54d86d012eb25
dev-mojtaba <mojtaba.dev.5858@gmail.com>
2022-07-28 18:09:40 +0430

    added initialization.js to edit somthing in page

---
+++ src/js/initialization.js
@@ -0,0 +1,19 @@
+/**variables */
+const sendBtn = document.querySelector('#send');
+
+
+
+/**events */
+document.addEventListener('DOMContentLoaded', initilizationPage);
+
+
+
+
+/**functions */
+function initilizationPage() {
+    // disable send button until fields get requirement value
+    sendBtn.disabled = true;
+}
+
+
+
